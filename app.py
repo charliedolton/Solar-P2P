9f926e5d0f2afcd697147b0c22dbfcfb014ee8e0
David Gaeta <dinkwho@gmail.com>
2022-02-25 13:06:35 -0800

    Adding firestore implementation to server

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Initialize Firestore DB
+cred = credentials.Certificate("solar_key.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+planet_ref = db.collection('Planets')
 
 app = Flask(__name__)
 
