75b27c21267dad901f8ff5771b90d43f73cad7c6
Weixuan Jia <56487470+jiaweixuan@users.noreply.github.com>
2022-05-07 19:31:24 -0700

    Added Dropdown Menu

--- index.js
+++ index.js
@@ -1,27 +1,76 @@
 const express = require("express");
-
+const res = require("express/lib/response");
+const fetch = require("node-fetch");
 const app = express();
+const fs = require("fs");
+const request = require('request');
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 //routes
+var URL = "https://solar-cst499.herokuapp.com";
+var DIR = "../register";
 
+
+var planets;
+request(URL + "/planetsInSystem?systemNum=1", function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        planets = JSON.parse(body);
+    }
+});
 // the movie list
 app.get('/', (req, res) => {
-    res.render('login');
+    let login = req.query.login;
+
+    console.log(planets);
+    res.render('login', { 'login': login, "planets" : planets });
 });
 
-app.post('/login', async(req, res)=>{
+const updateServer = async (url, data) => {
+    console.log(data);
+    var clientServerOptions = {
+        uri: URL + url,
+        body: data,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    return request(clientServerOptions, (error, response) => {
+        console.log(error, response.body);
+        return;
+    });
+}
+
+app.post('/login', async (req, res) => {
     let solarGroup = req.body.solarGroup;
-    let username = req.body.username;
+    let planetName = req.body.username;
     let password = req.body.password;
 
-    // TODO: use Api to login
-    console.log(solarGroup, username, password);
+    let data = {"planetName": planetName,"password": password};
 
+
+    let response = request.post(URL + "/login", params = data);
+    console.log(response)
+    // if (response.status != 200) {
+    //     res.redirect('/?login=failed');
+    // } else {
+    //     console.log(data);
+    //     if (!fs.existsSync(DIR)) {
+    //         fs.mkdirSync(DIR);
+    //     }
+    //     fs.writeFile(DIR + '/register.txt', data, (error) => {
+    //         if (error) {
+    //             throw error;
+    //         }
+    //     })
+    //     res.redirect('/');
+    // }
     res.redirect('/');
+    // TODO: use Api to login && save solarGroup and username
+
 });
 
 app.get('/register', (req, res) => {
@@ -32,12 +81,17 @@ app.get('/new/user', (req, res) => {
     res.render('newUser');
 });
 
-app.post('/new/user', (req, res) => {
+app.post('/new/user', async (req, res) => {
     let group = req.body.group;
     let username = req.body.username;
     let password = req.body.password;
 
-    console.log(group, username, password);
+    request.post({ headers: {'content-type' : 'application/json'}
+               , url: URL, body: data }
+               , function(error, response, body){
+        console.log(body); 
+    }); 
+    console.log(response);
 
     res.redirect('/new/user');
 });
